60884f96246d917e8785c2b914c1a42afc5c0ea2
ruxxzebre <paullaboratory@gmail.com>
2021-07-27 01:14:42 +0300

    fix eslint stubbing my brain

--- packages/shared/.eslintrc.js
+++ packages/shared/.eslintrc.js
@@ -2,8 +2,13 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    es6: true
   },
   extends: ["eslint:recommended"],
+  parserOptions: {
+    parser: "babel-eslint",
+    ecmaVersion: 2017,
+  },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
